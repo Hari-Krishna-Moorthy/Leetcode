04ecb19251fb9c8d12a5f91c78c170e336ef59e4
theighari <harikrishna.moorthy@shopup.org>
2022-08-08 08:10:10 +0530

    add two sum 2 input array is sorted

---
+++ 167/TwoSumII-Input-Array-Is-Sorted/code.java
@@ -0,0 +1,13 @@
+// URL : https://leetcode.com/problems/two-sum-ii-input-array-is-sorted/
+
+class Solution {
+    public int[] twoSum(int[] numbers, int target) {
+        Map<Integer, Integer> map = new HashMap<>();
+        for(int i=0;i<numbers.length;i++) {
+            if(map.containsKey(numbers[i])) 
+                return new int[] {map.get(numbers[i]) + 1, i+1};
+            map.put(target-numbers[i], i);
+        }
+        return null;
+    }
+}
